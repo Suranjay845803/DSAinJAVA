1743604d079832276e71d0b31de0ae62dde27c13
Suranjay845803 <suranjay.singh@gmail.com>
2022-12-28 00:16:16 +0530

    linear search

--- src/com/suranjay/dsa/search/LinearSearch.java
+++ src/com/suranjay/dsa/search/LinearSearch.java
@@ -4,17 +4,31 @@ public class LinearSearch {
 
     public static void main(String[] args) {
         int arr[] = {3,4,6,78,2};
+        int arr1[] = {};
         int res = linearSearch(arr, 78);
         System.out.println(res);
+        boolean search = linearSearch("Suranjay", "N");
+        System.out.println(search);
     }
 
     // Time complexity = best case O(1) , worst case O(n)
     public static int linearSearch(int[] arr, int number){
+        if(arr.length ==0)
+           return -1;
         for (int i = 0; i < arr.length; i++){
             if(arr[i] == number){
-                return i;
+                return arr[i];
             }
         }
         return -1;
+    }
+
+    public static boolean linearSearch(String str, String searchChar){
+        if(str.length()==0)
+            return  false;
+        if (str.indexOf(searchChar)> -1)
+            return  true;
+        return  false;
+
     }
 }
