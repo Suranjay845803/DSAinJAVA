18b2e963df14c46ff70f55297c596c4631e78238
Suranjay845803 <suranjay.singh@gmail.com>
2023-01-04 23:29:54 +0530

    binary search

--- src/com/suranjay/dsa/search/BinarySearch.java
+++ src/com/suranjay/dsa/search/BinarySearch.java
@@ -6,8 +6,12 @@ public class BinarySearch {
         int[] arr = {2,5,6,8,9,11};
         int res = binarySearch(arr, 6);
         System.out.println( res );
-    }
 
+        int[] arr1 = {223,89,56,34,6};
+        int res1 = binarySearchDesending(arr1, 6);
+        System.out.println( res1 );
+    }
+    // for sorted order in assending order
     private static int binarySearch(int[] arr, int target){
         int start = 0;
         int end = arr.length-1;
@@ -20,6 +24,23 @@ public class BinarySearch {
                 start = mid+1;
             }else{
                 end = mid-1;
+            }
+        }
+        return -1;
+    }
+
+    private static int binarySearchDesending(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+        while(start <= end){
+            int mid = (start + (end-start)/2);
+            int midEl = arr[mid];
+            if(target == midEl)
+                return mid;
+            if( target > arr[mid]) {
+                end = mid-1;
+            }else{
+                start = mid+1;
             }
         }
         return -1;
