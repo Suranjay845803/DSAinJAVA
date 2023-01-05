051e07517fcab801f1485520e844be683797b164
Suranjay845803 <suranjay.singh@gmail.com>
2023-01-05 20:57:48 +0530

    binary search

--- src/com/suranjay/dsa/search/CeilingNumber.java
+++ src/com/suranjay/dsa/search/CeilingNumber.java
@@ -6,41 +6,41 @@ public class CeilingNumber {
         int arr[] ={1,2,3,5,6,7,8,9,10,11,13,15,16};
         int arr1[] ={16,14,11,10,9,8,7,6,5,4,3,2,1};
         int arr2[] ={18,16,14,11,10,9,8,7,6,5,3,2,1};
-        int res = ceilingNumber(arr1, 13);
+        int res = ceilingNumber(arr, 14);
         System.out.println( res );
     }
 
     private static int ceilingNumber(int[] arr, int target){
         int start = 0;
         int end = arr.length-1;
-        int num = 0;
+       // int num = 0;
         boolean isAss = arr[end] > arr[0] ? true : false;
         while (start <= end ){
             int mid = (start + (end-start)/2);
             if (target == arr[mid])
-                num = arr[mid];
+                return arr[mid];
             if(isAss){
                 if(target > arr[mid]){
                     start = mid+1;
-                    if( arr[start] > target)
-                        return arr[start];
+                   /* if( arr[start] > target)
+                        return arr[start];*/
                 }else{
                     end = mid -1;
-                    if( arr[start] > target)
-                        return arr[start];
+                    /*if( arr[start] > target)
+                        return arr[start];*/
                 }
             }else{
                 if( target > arr[mid] ){
                     end = mid-1;
-                    if( arr[end] > target)
-                        return arr[end];
+                    /*if( arr[end] > target)
+                        return arr[end];*/
                 }else{
                   start = mid +1;
-                    if( arr[end] > target)
-                        return arr[end];
+                  /*  if( arr[end] > target)
+                        return arr[end];*/
                 }
             }
         }
-        return num;
+        return isAss ? arr[start] : arr[end];
     }
 }
